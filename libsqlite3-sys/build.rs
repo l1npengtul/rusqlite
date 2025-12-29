af5962eb8804810d488e9289978763fcd382471d
l1npengtul <l1npengtul@protonmail.com>
2025-12-30 05:53:03 +0900

    remove more flags

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -89,14 +89,10 @@ mod build_bundled {
         cfg.file(format!("{lib_name}/sqlite3.c"))
             .flag("-DSQLITE_CORE")
             .flag("-DSQLITE_DEFAULT_FOREIGN_KEYS=1")
-            .flag("-DSQLITE_ENABLE_API_ARMOR")
             .flag("-DSQLITE_ENABLE_COLUMN_METADATA")
             .flag("-DSQLITE_ENABLE_DBSTAT_VTAB")
             .flag("-DSQLITE_ENABLE_FTS5")
-            .flag("-DSQLITE_ENABLE_LOAD_EXTENSION=1")
-            .flag("-DSQLITE_ENABLE_MEMORY_MANAGEMENT")
             .flag("-DSQLITE_ENABLE_RTREE")
-            .flag("-DSQLITE_THREADSAFE=1")
             .flag("-DSQLITE_OMIT_WAL=1")
             .warnings(false);
 
